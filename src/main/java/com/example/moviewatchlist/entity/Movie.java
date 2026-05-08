ad90cd58e1f8d9d90c3ed9d0fb240a3ee7fddb99
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-05-08 16:13:04 -0700

    movie.java

--- src/main/java/com/example/moviewatchlist/entity/Movie.java
+++ src/main/java/com/example/moviewatchlist/entity/Movie.java
@@ -1,4 +1,20 @@
 package com.example.moviewatchlist.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "movies")
+@Data
 public class Movie {
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String title;
+    private String genre;
+    private int releaseYear;
+    private boolean isWatched;
+    private Double rating;
 }
