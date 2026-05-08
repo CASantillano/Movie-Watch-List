d226f6db08d614cdd1a56b693ee42bdb9f469fb3
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-05-08 16:34:45 -0700

    Repository files

--- src/main/java/com/example/moviewatchlist/repository/UserRepository.java
+++ src/main/java/com/example/moviewatchlist/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.moviewatchlist.repository;
 
-public class UserRepository {
+import com.example.moviewatchlist.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
